02949e7027c3476bcca72900fc5097f8983df712
Tomas Nilsen Nakkerud <74247764+tommenbro@users.noreply.github.com>
2022-07-10 02:30:47 +0200

    hero and nav

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Hero from "../components/Hero";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -13,7 +14,10 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Hero />
+      <Hero
+        heading="Norse Photofolio"
+        message="Norways finest moments captured for you to injoy."
+      />
     </div>
   );
 }
